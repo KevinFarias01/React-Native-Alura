0992fcb27e49e3886a89abd5315be9736755ce28
Kevin Farias <kevin.farias.rib@gmail.com>
2022-05-19 21:11:44 -0300

    1

--- OrgsCesta/App.js
+++ OrgsCesta/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Cesta from '../src/telas/Cesta';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Iniciei ESSA PORRA!!!!! FINALMENTEEEE!!!!</Text>
+      <Text>É foda.....</Text>
       <StatusBar style="auto" />
     </View>
   );
