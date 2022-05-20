c72e6c4ad992ee0b226d5bc9379aa9947d773e70
Kevin Farias <kevin.farias.rib@gmail.com>
2022-05-20 10:50:28 -0300

    10

--- OrgsCesta/App.js
+++ OrgsCesta/App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
 
 import './src/telas/Cesta';
 import Cesta from './src/telas/Cesta';
@@ -8,18 +7,10 @@ import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
